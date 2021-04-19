7de8778ce3b86ed9ea0c00c50a73adfa4f81ee69
Pondi1015 <pondi20902@gmail.com>
2021-04-19 16:37:53 +0800

    物聯網課堂學習用

---
+++ IOT_test.ino
@@ -0,0 +1,110 @@
+#include "WiFiEsp.h"
+#include "secrets.h"
+#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+#include <Streaming.h>
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiEspClient  client;
+
+
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+// Initialize our values
+float One = 0;
+float Two = 0;
+String Three = "";
+String myStatus = "";
+
+void setup() {
+  //Initialize serial and wait for port to open
+  Serial.begin(9600);  // Initialize serial
+  while(!Serial){
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+  
+  // initialize serial for ESP module  
+  setEspBaudRate(9600);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+
+  Serial.print("Searching for ESP8266..."); 
+  // initialize ESP module
+  WiFi.init(&Serial1);
+
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+  Serial.println("found it!");
+   
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+}
+
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+
+  // set the fields with the values
+  ThingSpeak.setField(1, One);
+  ThingSpeak.setField(2, Two);
+  ThingSpeak.setField(3, Three);
+
+
+  // write to the ThingSpeak channel
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+ 
+  // change the values
+  One++;
+  if(One > 99){
+    One = 0;
+  }
+  Two = random(0,100);
+
+  delay(15000); // Wait 20 seconds to update the channel again
+  
+}
+
+// This function attempts to set the ESP8266 baudrate. Boards with additional hardware serial ports
+// can use 115200, otherwise software serial is limited to 19200.
+void setEspBaudRate(unsigned long baudrate){
+  long rates[6] = {115200,74880,57600,38400,19200,9600};
+
+  Serial.print("Setting ESP8266 baudrate to ");
+  Serial.print(baudrate);
+  Serial.println("...");
+
+  for(int i = 0; i < 6; i++){
+    Serial1.begin(rates[i]);
+    delay(100);
+    Serial1.print("AT+UART_DEF=");
+    Serial1.print(baudrate);
+    Serial1.print(",8,1,0,0\r\n");
+    delay(100);  
+  }
+    
+  Serial1.begin(baudrate);
+}
---
+++ secrets.h
@@ -0,0 +1,6 @@
+#define SECRET_SSID "AP-401-2.4G"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "class401"  // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1362992     // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "5XITS4GAXF99IQWF"   // replace XYZ with your channel write API Key
+#define SECRET_READ_APIKEY "LKRFHUEGLKCCJQ9Z"
